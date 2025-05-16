a5868397c7616e4dfc481b50ad4d18a31f234c68
hamza <hamza.m.soliman47@gmail.com>
2025-05-16 18:31:23 +0300

    fix: enhance error handling and debugging in test framework

--- cypress/pages/BasePage.js
+++ cypress/pages/BasePage.js
@@ -1,31 +1,77 @@
 class BasePage {
     constructor() {
         this.url = '';
+        this.defaultWaitTime = 10000; // 10 seconds
+    }
+
+    log(message) {
+        cy.log(`[${this.constructor.name}] ${message}`);
     }
 
     visit() {
-        cy.visit(this.url);
+        this.log(`Visiting URL: ${this.url}`);
+        cy.visit(this.url, {
+            timeout: 30000, // 30 seconds
+            retryOnNetworkFailure: true,
+            retryOnStatusCodeFailure: true,
+            retryOnTimeout: true,
+            retryAttempts: 3
+        });
+        cy.url().should('include', this.url);
     }
 
-    getElement(selector) {
-        return cy.get(selector);
+
+    getElement(selector, options = {}) {
+        const { timeout = this.defaultWaitTime } = options;
+        this.log(`Getting element: ${selector}`);
+        return cy.get(selector, { timeout }).should('be.visible');
     }
 
-    clickElement(selector) {
-        this.getElement(selector).click();
+    clickElement(selector, options = {}) {
+        const { force = false } = options;
+        this.log(`Clicking element: ${selector}`);
+        this.getElement(selector).click({ force });
     }
 
-    typeText(selector, text) {
-        this.getElement(selector).type(text);
+    typeText(selector, text, options = {}) {
+        const { clear = true, force = false } = options;
+        this.log(`Typing '${text}' into: ${selector}`);
+        const element = this.getElement(selector);
+        if (clear) {
+            element.clear({ force });
+        }
+        element.type(text, { force });
     }
 
-    shouldBeVisible(selector) {
-        this.getElement(selector).should('be.visible');
+    shouldBeVisible(selector, options = {}) {
+        const { timeout = this.defaultWaitTime } = options;
+        this.log(`Checking visibility of: ${selector}`);
+        return this.getElement(selector, { timeout }).should('be.visible');
     }
 
-    shouldContainText(selector, text) {
-        this.getElement(selector).should('contain', text);
+    shouldContainText(selector, text, options = {}) {
+        const { timeout = this.defaultWaitTime } = options;
+        this.log(`Checking '${selector}' contains text: ${text}`);
+        return this.getElement(selector, { timeout }).should('contain', text);
+    }
+
+    waitForPageLoad() {
+        this.log('Waiting for page to load...');
+        cy.document().should('exist');
+        cy.get('body').should('be.visible');
+        cy.window().should('have.property', 'jQuery');
+    }
+
+    waitForNetworkIdle() {
+        this.log('Waiting for network to be idle...');
+        cy.intercept('**').as('anyRequest');
+        cy.wait('@anyRequest', { timeout: 10000 });
+    }
+
+    scrollTo(selector) {
+        this.log(`Scrolling to: ${selector}`);
+        this.getElement(selector).scrollIntoView();
     }
 }
 
-export default BasePage; 
+export default BasePage;
